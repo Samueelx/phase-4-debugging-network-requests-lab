e04e915311f6527e0932ef19024a160f31c7075c
Samueelx <gsammygitau@gmail.com>
2023-03-21 22:34:13 +0300

    done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_095050) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_05_09_095050) do
   create_table "toys", force: :cascade do |t|
     t.string "name"
     t.string "image"
     t.integer "likes"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
